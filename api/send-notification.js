1939de5199cc53f22dac5f725fac81edc9e1e5d8
naitiksaxena06-design <naitiksaxena06@gmail.com>
2026-07-10 16:34:59 +0530

    Update send-notification.js

--- api/send-notification.js
+++ api/send-notification.js
@@ -26,7 +26,10 @@ export default async function handler(req, res) {
   try {
     await admin.messaging().send({
       token,
-      notification: { title, body: body || '' },
+      data: {
+        title,
+        body: body || '',
+      },
     });
     res.status(200).json({ success: true });
   } catch (err) {
